51880abb2965f8cc0d03fc957cfccdc2098ce2df
vincentayorinde <vincentayorinde@yahoo.com>
2023-03-13 03:16:28 +0000

    update text

--- index.js
+++ index.js
@@ -86,9 +86,7 @@ app.get('/auth/slack/callback', async (req, res) => {
     res
       .status(200)
       .send(
-        `<html><body><p>You have successfully logged in with your slack account! Here are the details:</p><p>Response: ${JSON.stringify(
-          response,
-        )}</p></body></html>`,
+        `<html><body><p>You have successfully logged integrated Alerta to the slack channel! Enjoy!</p></body></html>`,
       );
   } catch (err) {
     console.log(err);
